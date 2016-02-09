3e838b6e532191e2be9670f36f11899e60cb200a
robert-murray <robertmurray@192-168-1-2.tpgi.com.au>
2016-02-09 22:04:36 +1100

    Added slide-in nav links, CSS changes, added timeline view

--- app/assets/javascripts/slideinmenu.js
+++ app/assets/javascripts/slideinmenu.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 
-  var menuLeft = document.getElementById('cbp-spmenu-s1'),
+  var menuLeft = document.getElementById('spmenu-s1'),
     showLeftPush = document.getElementById('showLeftPush'),
     body = document.body;
 
   showLeftPush.onclick = function() {
     classie.toggle(this, 'active');
-    classie.toggle(body, 'cbp-spmenu-push-toright');
-    classie.toggle(menuLeft, 'cbp-spmenu-open');
+    classie.toggle(body, 'spmenu-push-toright');
+    classie.toggle(menuLeft, 'spmenu-open');
     disableOther('showLeftPush');
   };
 
